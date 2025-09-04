0de5bc9bede8fcab453063c7d94da0deecbb07e7
solsticexin <solstice.xin@gmail.com>
2025-09-04 13:11:37 +0800

    feat(link-list): 添加链表插入、删除、头插、尾插和遍历功能

--- core/include/list.h
+++ core/include/list.h
@@ -11,6 +11,11 @@ typedef struct SqList {
     int data[MaxSize];
     int length;
 }SqList;
+void Traverse(SqList& l);
+bool A19_01(SqList& l,int& e);
+void A19_02(SqList& l);
+void A19_03(SqList& l,int x);
+bool A19_04(SqList& l,int s,int t);
 namespace link_list {
     typedef int ElemType;
     typedef struct LNode{
@@ -21,10 +26,10 @@ namespace link_list {
     int length(LinkList& L);
     LNode* GetElem(LinkList& L,int i);
     LNode* LocateElem(LinkList& L,ElemType e);
+    bool ListInsert(LinkList& L,int i,ElemType e);
+    bool ListDelete(LinkList& L, int i,ElemType& e);
+    LinkList List_HeadInsert(LinkList& L);
+    LinkList List_TailInsert(LinkList& L);
+    void Traverse(LinkList& L);
 }
-void Traverse(SqList& l);
-bool A19_01(SqList& l,int& e);
-void A19_02(SqList& l);
-void A19_03(SqList& l,int x);
-bool A19_04(SqList& l,int s,int t);
 #endif //TEST_CPP_DATASTRUCTURE_LIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "chapter-2/include/chapter-2.h"
 #include "chapter-3/include/chapter-3.h"
 #include "core/include/string.h"
+#include "core/include/list.h"
 int main() {
     test_A19_01();
     test_A19_02();
@@ -10,4 +11,7 @@ int main() {
     test_A19_06();
     string::test_Index();
     test_A19_12();
+    link_list::LNode* L;
+    link_list::List_TailInsert(L);
+    link_list::Traverse(L);
 }
